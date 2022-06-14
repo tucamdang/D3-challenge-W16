40aceb21e1e00a5db41ce5d96018a11c422fde06
tucamdang <99168697+tucamdang@users.noreply.github.com>
2022-06-14 17:06:19 +0930

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,30 @@
 
-    var svgWidth = 960;
-    var svgHeight = 500;
-    var margin = {
-      top: 20,
-      right: 40,
-      bottom: 80,
-      left: 100
-    };
+var svgWidth = 960;
+var svgHeight = 500;
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
 
 
-    var width = svgWidth - margin.left - margin.right;
-    var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
-    var svg = d3
-        .select("#scatter")
-        .append("svg")
-        .attr("width", svgWidth)
-        .attr("height", svgHeight)
-        
-  
-  // Append an SVG group
-  var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-  
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
   // Set initial parmes
 var XAxis = "poverty";
 var YAxis = "healthcare";
